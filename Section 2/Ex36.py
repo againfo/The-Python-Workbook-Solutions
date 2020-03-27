3c072df5383102cc70a154b699c7e668fc30ddcc
againfo <51507449+againfo@users.noreply.github.com>
2020-03-27 20:21:58 +0100

    Update Ex36
    Hi you missed the second choice ( letter y)

--- Section 2/Ex36.py
+++ Section 2/Ex36.py
@@ -2,6 +2,8 @@ letter = input("Please enter a letter of the alphabet: ").lower()
 
 if letter in "aeiou":
 	print("This letter is a vowel.")
+elif letter==y:
+	print("this letter is sometimes a vowel ... sometimes a consonant"
 else:
 	print("This letter is a consonant.")
 	
